2ddc8980f3722d27c7b4b08ce1b188a1f97bd282
alp55 <92816090+alp55@users.noreply.github.com>
2022-03-25 18:29:22 +0300

    Update klavyeden_girilen_iki_sayının_toplamı.py

--- klavyeden_girilen_iki_sayının_toplamı.py
+++ klavyeden_girilen_iki_sayının_toplamı.py
@@ -1,4 +1,3 @@
-
 x=input('birinci sayıyı giriniz :') #iki sayıyı toplamak için iki degişken tanımladık
 y=input('ikinci sayıyı giriniz :') #tanımladıgımız x ve y degişkenine input ile veri girişi yaptık
 t=int(x)+int(y) #girdigimiz bu verilerin int türünde yani sayısal bir veri oldugunu belirtik ve t degişkeni üzerinde iki degişkeni toplatık
