d079f6badaa44d1c1588bdd441916e659fd19217
alp55 <92816090+alp55@users.noreply.github.com>
2022-03-25 22:59:12 +0300

    Add files via upload

---
+++ print_yazdırma_methodları.py
@@ -0,0 +1,6 @@
+a=5
+b=6
+print("%s ile %s carpılırsa %s elde edilir"%(a,b,a*b)) # "%s" koydugumuz yerlere sol baştan başlayarak degişkenler yerleştirilir
+cumle="{} ile {} carpılırsa {} elde edilir" 
+print(cumle.format(a,b,a*b)) # güzel parantezin içine format komutu ile degişken yazdırmak mümkündür
+print(a*b) # degişkenler bu şekilde direk print içine yazdırılıp dört işlem yapmak mümkündür
